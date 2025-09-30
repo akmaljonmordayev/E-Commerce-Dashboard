4343ff8ca4971479b05fdec400619fc7a8e06a73
AlfaCefOf <alfaxcef@gmail.com>
2025-09-30 14:21:32 +0500

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import React from "react";
+import Header from "./bigComponents/header/header";
 function App() {
   return (
     <>
-     <h1>
-      ADMIN DASHBOARD
-     </h1>
+      <h1>ADMIN DASHBOARD</h1>
+      <Header/>
     </>
   );
 }
