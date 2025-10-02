622817f43d3d83f4f3f9a9e1e14a78c990bd6f7b
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-10-02 17:25:02 +0500

    profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-
-function Profile() {
-  return (
-    <div>
-
 import Header from "../../bigComponents/header/header";
+
 function Profile() {
   return (
     <div>
       <Header />
-
       <h1>Profile page</h1>
     </div>
   );
