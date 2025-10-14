1dd8a5c29bd0c4aa3376afa0f50b7d8c533d012e
AlfaCefOf <alfaxcef@gmail.com>
2025-10-14 17:21:44 +0500

    added

--- src/pages/Archieve/Archieve.jsx
+++ src/pages/Archieve/Archieve.jsx
@@ -1,46 +1,74 @@
-import { Outlet, Link } from "react-router-dom";
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 import ProductCard from "../../bigComponents/productCard";
-function Archieve() {
+import useGet from "../../customHooks/useGet";
+import Loader from "../../smallComponents/loader";
+import ErrorMessage from "../../bigComponents/errorMessage";
+
+export default function Archieve() {
+  const {
+    data: archivedProducts,
+    loading: loadingProducts,
+    error: errorProducts,
+  } = useGet("/productsArchieve");
+
+  const {
+    data: archivedCustomers,
+    loading: loadingCustomers,
+    error: errorCustomers,
+  } = useGet("/customersArchieve");
+
+  const {
+    data: archivedOrders,
+    loading: loadingOrders,
+    error: errorOrders,
+  } = useGet("/ordersArchieve");
+
+  const {
+    data: archivedUsers,
+    loading: loadingUsers,
+    error: errorUsers,
+  } = useGet("/usersArchieve");
+
+  if (loadingProducts || loadingCustomers || loadingOrders || loadingUsers)
+    return <Loader />;
+
+  if (errorProducts || errorCustomers || errorOrders || errorUsers)
+    return <ErrorMessage message="Error" />;
+
   return (
-    <div>
-      <div className="flex gap-[30px]">
-        <Link to="productsarchieve">
+    <div className="p-6 text-white">
+      <div className="flex gap-[30px] justify-evenly flex-wrap">
+        <Link to="/archieve/productsarchieve">
           <ProductCard
-            height={150}
-            width={280}
-            title={"Products Archieve"}
-            count={0}
+            title="Archived Products"
+            count={archivedProducts ? archivedProducts.length : 0}
           />
         </Link>
-        <Link to="usersarchieve">
+
+        <Link to="/archieve/customersarchieve">
           <ProductCard
-            height={150}
-            width={280}
-            title={"Users Archieve"}
-            count={0}
+            title="Archived Customers"
+            count={archivedCustomers ? archivedCustomers.length : 0}
           />
-        </Link>{" "}
-        <Link to="customersarchieve">
+        </Link>
+
+        <Link to="/archieve/ordersarchieve">
           <ProductCard
-            height={150}
-            width={280}
-            title={"Customers Archieve"}
-            count={0}
+            title="Archived Orders"
+            count={archivedOrders ? archivedOrders.length : 0}
           />
-        </Link>{" "}
-        <Link to="ordersarchieve">
+        </Link>
+
+        <Link to="/archieve/usersarchieve">
           <ProductCard
-            height={150}
-            width={280}
-            title={"Orders Archieve"}
-            count={0}
+            title="Archived Users"
+            count={archivedUsers ? archivedUsers.length : 0}
           />
-        </Link>{" "}
+        </Link>
       </div>
+
       <Outlet />
     </div>
   );
 }
-
-export default Archieve;
